0248fa56fd009aca45540b58ad5c0a007a725fce
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-02-27 20:36:35 +0000

    Direct Message model

--- app/Models/DMChannel.php
+++ app/Models/DMChannel.php
@@ -9,13 +9,6 @@ class DMChannel extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['dm_channel_id', 'sender_id', 'content'];
 
-   public function dmChannel() {
-       return $this->belongsTo(DMChannel::class);
-   }
 
-    public function sender() {
-        return $this->belongsTo(User::class, 'sender_id');
-    }
 }
--- app/Models/DirectMessage.php
+++ app/Models/DirectMessage.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class DirectMessage extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['dm_channel_id', 'sender_id', 'content'];
+
+    public function dmChannel() {
+        return $this->belongsTo(DMChannel::class);
+    }
+
+    public function sender() {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
 }
