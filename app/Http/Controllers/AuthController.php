3ea70256b2711730a27f5fe4af2261564ae94a47
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-02 21:10:45 +0000

    beginning of reg controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use Spatie\Permission\Models\Role;
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request) {
+
+        $user = User::create([
+            'name' => 'required|min:1|max:50',
+            'email' => 'required|email|unique:users,email',
+            'password'
+        ]);
+    }
 }
