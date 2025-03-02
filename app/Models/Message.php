9aaab229ed4eb031ef3c47a8fc8b6048e5e22d09
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-02 20:19:32 +0000

    models

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -9,6 +9,17 @@ class Message extends Model
 {
     use HasFactory;
 
+   protected $fillable = ['sender_id', 'receiver_id', 'content'];
 
+   public function sender() {
+       return $this->belongsTo(User::class, 'sender_id');
+   }
 
+   public function receiver() {
+      return $this->belongsTo(User::class, 'receiver_id');
+   }
+
+   public function Server() {
+      return $this->belongsTo(Server::class, 'server_id');
+   }
 }
--- app/Models/Server.php
+++ app/Models/Server.php
@@ -9,5 +9,18 @@ class Server extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'server_id', 'owner_id'];
+
+    public function owner() {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function member() {
+      return $this->hasMany(User::class, 'server_member');
+    }
+
+    public function messages() {
+       return $this->hasMany(Message::class);
+    }
 
 }
