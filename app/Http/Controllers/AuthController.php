bc93728ceabc84f5081664b32fcb593c4aac664d
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-02 22:32:42 +0000

    has passowrd on registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,17 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
     public function register(Request $request) {
         $credentials = $request->validate([
             'name' => 'required|string|min:1|max:30',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:5|max:30|confirmed',
+            'password' => Hash::make('required|string|min:5|max:30|confirmed'),
         ]);
 
         User::create($credentials);
         return route('login');
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->validate([
+           'email' => 'required|email',
+           'password'
+        ]);
     }
 
 }
