6f64d38f7db937d7814f2e33b916f838478235c2
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-03 22:20:37 +0000

    message policy

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Message;
+use Spatie\Permission\Models\Role;
+use App\Models\Server;
+
+class MessageController extends Controller
+{
+    public function sendMessage(Request $request, Server $server) {
+        $this->authorize('sendMessage', $server);
+
+
+
+    }
+}
