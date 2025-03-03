427d611820e589f52413b506cb2993fee2089064
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-03 20:08:01 +0000

    logout method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,13 +23,19 @@ class AuthController extends Controller
     public function login(Request $request) {
         $credentials = $request->validate([
            'email' => 'required|email',
-           'password' =>  Hash::make('required|string|min:5|max:30'),
+           'password' =>  'required|string|min:5|max:30',
         ]);
         if(!Auth::attempt($credentials)) {
             return back()->withErrors(['Email or password is invalid']);
         }
         $session = $request->session()->regenerate();
         return route('dashboard');
+    }
+
+    public function logout(Request $request) {
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('login');
     }
 
 }
