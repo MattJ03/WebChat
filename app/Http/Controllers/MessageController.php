de2d6aef3a796748008918afd1e087852424aeb5
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-05 22:48:59 +0000

    fixed issue with property not being visible by using the input method

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,19 +6,22 @@ use Illuminate\Http\Request;
 use App\Models\Message;
 use Spatie\Permission\Models\Role;
 use App\Models\Server;
-
+use Illuminate\Support\Facades\Auth;
+use App\Events\MessageSent;
 class MessageController extends Controller
 {
-    public function sendMessage(Request $request, Server $server) {
-        $this->authorize('sendMessage', $server);
 
+    public function store(Request $request, Server $server) {
         $request->validate([
-           'content' => 'required|max:200',
+           'content' => 'required|string|max:250',
         ]);
 
+        $message = $server->messages()->create([
+           'user_id' => Auth::id(),
+            'content' => $request->input('content'),
 
-
-
-
+        ]);
+        broadcast(new MessageSent($message))->toOthers();
+        return response()->json(['message' => $message, 'Message sent successfully']);
     }
 }
