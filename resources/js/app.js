b8fba3ed1b9bc4fa93f5f2af4883aa9f85deec55
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-09 22:48:25 +0000

    app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './index';
+
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount('#app');
