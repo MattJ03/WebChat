e326a6240dff5db4638bb1180dc83869da30f4ec
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-13 21:44:39 +0000

    vue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,3 +1,12 @@
-@vite(['resources/js/app.js']);
-
-<div id="app"></div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat App</title>
+    @vite('resources/js/app.js') {{-- Load Vue App --}}
+</head>
+<body>
+<div id="app" data-messages='@json($messages)' data-server-id="{{ $server->id }}"></div>
+</body>
+</html>
